877af424be2eb7c3c9fefeeb0204c69723a13035
Ryker Fish <facepallm16@gmail.com>
2020-05-17 21:06:13 -0600

    Small changes

--- LinkFileBot.py
+++ LinkFileBot.py
@@ -1,39 +1,38 @@
 import praw
-import datetime
 import time
+import ContentFinder
 
 
 
-accountFile = open("./exclude/account.txt", "r")
-privateUsername = accountFile.readline().rstrip()
-privatePassword = accountFile.readline().rstrip()
-accountFile.close()
+def main():
 
-reddit = praw.Reddit(client_id = 'mquSmzl3A6nnSg',
- client_secret = 'ayz-KjEA2SkzCENTS5RvTaRpf24',
-  username = privateUsername,
-   password = privatePassword,
-    user_agent = 'Link identification script (by /u/failcrab)')
+    accountFile = open("./exclude/account.txt", "r")
+    privateUsername = accountFile.readline().rstrip()
+    privatePassword = accountFile.readline().rstrip()
+    accountFile.close()
 
-# skip_existing=True skips any comments that were posted before the stream
-# was created
-"""
-for comment in reddit.subreddit('Test').stream.comments(skip_existing=True):
-    if(any(x in comment.body.lower() for x in PhrasesToLookFor)):
+    reddit = praw.Reddit(client_id = 'mquSmzl3A6nnSg',
+     client_secret = 'ayz-KjEA2SkzCENTS5RvTaRpf24',
+      username = privateUsername,
+       password = privatePassword,
+        user_agent = 'Link identification script (by /u/LinkFileBot)')
+
+    # skip_existing=True skips any comments that were posted before the stream
+    # was created
+    for comment in reddit.subreddit('Test').stream.comments(skip_existing=True):
         try:
-            comment.reply(MyReply)
+            # grabs link from comment body
+            linkIndex = comment.body_html.find("href")
+            linkEndIndex = comment.body_html.find("\">http", linkIndex)
+            link = comment.body_html[linkIndex + 6:linkEndIndex]
+            print(link)
+
+            text = ContentFinder.GenerateLinkDescription(link)
+            comment.reply(text)
+
         except:
-            print("\n!! failed due to timeout")
-            pass
-"""
-#skip_existing=True
-counter = 0
-for comment in reddit.subreddit('Test').stream.comments(skip_existing=True):
-    try:
-        print("HTML BODY: " + comment.body_html + "\n")
-        linkIndex = comment.body_html.find("href")
-        linkEndIndex = comment.body_html.find("\">http", linkIndex)
-        link = comment.body_html[linkIndex + 6:linkEndIndex]
-        print(link)
-    except:
-        continue
+            continue
+
+
+
+main()
